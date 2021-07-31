a39285be572f11892f0c134c5afb10180d33ab11
Sandro <sandrofillinich@gmail.com>
2021-07-31 16:09:53 +0100

    feat: create belongTo relation between Todo > TodoList

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,4 +1,5 @@
-import {Entity, model, property} from '@loopback/repository';
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {TodoList} from './todo-list.model';
 
 @model()
 export class Todo extends Entity {
@@ -25,10 +26,8 @@ export class Todo extends Entity {
   })
   isComplete?: boolean;
 
-  @property({
-    type: 'number',
-  })
-  todoListId?: number;
+  @belongsTo(() => TodoList)
+  todoListId: number;
 
   constructor(data?: Partial<Todo>) {
     super(data);
