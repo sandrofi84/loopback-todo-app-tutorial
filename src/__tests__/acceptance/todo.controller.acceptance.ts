41b3318fea3252ea1fb268318a86834839ad27c5
Sandro <sandrofillinich@gmail.com>
2021-08-01 22:08:21 +0100

    test: add test for POST /todos endpoint

--- src/__tests__/acceptance/todo.controller.acceptance.ts
+++ src/__tests__/acceptance/todo.controller.acceptance.ts
@@ -1,10 +1,25 @@
 import {Client, expect} from '@loopback/testlab';
 import {TodoListApplication} from '../..';
+import {DbDataSource} from '../../datasources';
+import {givenEmptyDB} from '../helpers/database.helpers';
+import {givenTodoData} from '../helpers/todo.helpers';
 import {setupApplication} from './test-helper';
 
-describe('TodoController', () => {
+
+describe('TodoController (acceptance)', () => {
+  const datasource = new DbDataSource();
+
   let app: TodoListApplication;
   let client: Client;
+
+
+  beforeEach('givenEmptyDatabase', async () => {
+    await givenEmptyDB(datasource);
+  })
+
+  afterEach('givenEmptyDatabase', async () => {
+    await givenEmptyDB(datasource);
+  })
 
   before('setup application', async () => {
     ({ app, client } = await setupApplication());
@@ -14,11 +29,32 @@ describe('TodoController', () => {
     await app.stop();
   });
 
-  it('invokes GET /todos', async () => {
-    const response = await client
-      .get('/todos')
-      .expect(200);
+  describe('GET /todos', () => {
+    it('returns an array of todos', async () => {
+      const response = await client
+        .get('/todos')
+        .expect(200);
 
-    expect(response.body).Array();
-  })
+      console.log(response.body);
+
+      expect(response.body).Array();
+    });
+  });
+
+  describe('POST /todos', () => {
+    it('creates a todo in the database', async () => {
+      const todoData = givenTodoData();
+
+      const response = await client
+        .post('/todos')
+        .send(todoData)
+        .set('Accept', 'application/json')
+        .expect(200);
+
+      expect(response.body).containDeep(todoData);
+      expect(response.body).to.have.property("id");
+    });
+  });
+
+
 })
