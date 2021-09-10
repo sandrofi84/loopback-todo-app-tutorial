b1cd77dc48ffdbacc315faeb6d3ebb2516c7f647
Sandro <sandrofillinich@gmail.com>
2021-09-10 15:07:46 +0100

    fix: fix Profile repository to fetch Pictures

--- src/repositories/profile.repository.ts
+++ src/repositories/profile.repository.ts
@@ -1,16 +1,33 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Profile, ProfileRelations} from '../models';
+import {Getter, inject} from '@loopback/core';
+import {
+  DefaultCrudRepository,
+  HasManyRepositoryFactory,
+  repository,
+} from '@loopback/repository';
+import {PictureRepository} from '.';
 import {DbDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import {Picture, Profile, ProfileRelations} from '../models';
 
 export class ProfileRepository extends DefaultCrudRepository<
   Profile,
   typeof Profile.prototype.id,
   ProfileRelations
 > {
+  public readonly pictures: HasManyRepositoryFactory<
+    Picture,
+    typeof Profile.prototype.id
+  >;
+
   constructor(
     @inject('datasources.db') dataSource: DbDataSource,
+    @repository.getter('PictureRepository')
+    protected pictureRepositoryGetter: Getter<PictureRepository>,
   ) {
     super(Profile, dataSource);
+    this.pictures = this.createHasManyRepositoryFactoryFor(
+      'pictures',
+      pictureRepositoryGetter,
+    );
+    this.registerInclusionResolver('pictures', this.pictures.inclusionResolver);
   }
 }
